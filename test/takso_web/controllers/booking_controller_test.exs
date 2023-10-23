67886cd804a9a8d1575887063e8987ab08e33244
Mushfug Soltanov <mushfug@ut.ee>
2023-10-23 23:55:22 +0300

    Part4

--- test/takso_web/controllers/booking_controller_test.exs
+++ test/takso_web/controllers/booking_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule TaksoWeb.BookingControllerTest do
   use TaksoWeb.ConnCase
-  alias Takso.{Repo,Sales.Taxi}
+  
+  alias Takso.Repo
+  alias Takso.Sales.{Taxi, Booking}
 
   test "Booking rejection", %{conn: conn} do
     Repo.insert!(%Taxi{status: "busy"})
@@ -15,6 +17,12 @@ defmodule TaksoWeb.BookingControllerTest do
     conn = get conn, redirected_to(conn)
     assert html_response(conn, 200) =~ ~r/Your taxi will arrive in \d+ minutes/
   end
-end
+
+   test "booking requires a 'pickup address'" do
+        changeset = Booking.changeset(%Booking{}, %{pickup_address: nil, dropoff_address: "Liivi 2"})
+        assert Keyword.has_key? changeset.errors, :pickup_address
+    end
+  end
+
 
 
