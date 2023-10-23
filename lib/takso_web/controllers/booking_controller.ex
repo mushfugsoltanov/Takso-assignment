fb12b79d37daf993452d2672490cfef426056d23
Mushfug Soltanov <mushfug@ut.ee>
2023-10-23 23:00:54 +0300

    Part3

--- lib/takso_web/controllers/booking_controller.ex
+++ lib/takso_web/controllers/booking_controller.ex
@@ -1,17 +1,30 @@
 defmodule TaksoWeb.BookingController do
   use TaksoWeb, :controller
 
-  def new(conn, _params ) do
+  import Ecto.Query, only: [from: 2]
+  alias Takso.Sales.Taxi
+  alias Takso.Repo
+
+  def new(conn, _params) do
     render conn, "new.html"
   end
 
-  def index(conn, _params ) do
+  def index(conn, _params) do
     render conn, "index.html"
-  end  
+  end
 
   def create(conn, _params) do
-    conn
-        |> put_flash(:info, "Your taxi will arrive in 20 minutes")
-        |> redirect(to: "/bookings")
+    query = from t in Taxi, where: t.status == "available", select: t
+
+    available_taxis = Repo.all(query)
+    if length(available_taxis)!=0 do
+      conn
+      |> put_flash(:info, "Your taxi will arrive in 15 minutes")
+      |> redirect(to: "/bookings")
+    else
+      conn
+      |> put_flash(:error, "All taxi drivers are busy. At present, there is no taxi available!")
+      |> redirect(to: "/bookings/new")
+    end
   end
 end
